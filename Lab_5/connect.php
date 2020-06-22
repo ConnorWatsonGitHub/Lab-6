9f81082efe0515278f6e74364ee824948fbe87a2
ConnorWatsonGitHub <66978864+ConnorWatsonGitHub@users.noreply.github.com>
2020-06-22 18:28:21 -0400

    Delete connect.php

--- Lab_5/connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $dsn = 'mysql:host=localhost;dbname=COMP1006_SUMMER2020';
-    $username = 'root';
-    $password = ''; 
-    $db = new PDO($dsn, $username, $password);
-    //set error mode to exception 
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-?>
